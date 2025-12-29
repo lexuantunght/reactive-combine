8fd934c0c76eb24364dc13cece3dd6f18df4da69
tunglx2 <tunglx2@vng.com.vn>
2025-12-29 14:15:14 +0700

    support multi sub

--- src/observer.ts
+++ src/observer.ts
@@ -3,32 +3,75 @@ import { Signal } from './signal';
 
 export function observe<T extends Object, K extends keyof T>(
     instance: T,
+    callback?: (data: T) => void
+): () => void;
+export function observe<T extends Object, K extends keyof T>(
+    instance: T,
     item: K,
-    callback: (data: T[K]) => void
+    callback?: (data: T[K]) => void
+): () => void;
+export function observe<T extends Object, K extends keyof T>(
+    instance: T,
+    item?: K,
+    callback?: (data: T[K]) => void
 ) {
+    if (!callback) {
+        throw Error('Callback cannot be null or undefined');
+    }
+
     const signals: Map<string, Signal<T[K]>> = Reflect.getMetadata(SIGNAL_KEY, instance);
     if (!signals) {
-        throw Error(`The instance which item ${String(item)} is not observable`);
+        throw Error(`The instance of ${instance.constructor.name} is not observable`);
     }
-    const signal = signals.get(String(item));
-    if (!signal) {
-        throw Error(`The item ${String(item)} is not reactive`);
+
+    if (item) {
+        const signal = signals.get(String(item));
+        if (!signal) {
+            throw Error(`The item ${String(item)} is not reactive`);
+        }
+        return signal.subscribe(callback);
     }
-    return signal.subscribe(callback);
+
+    const subs: Array<() => void> = [];
+    signals.forEach((signal) => {
+        subs.push(signal.subscribe(callback));
+    });
+    return () => {
+        subs.forEach((fn) => fn());
+    };
 }
 
 export function unobserve<T extends Object, K extends keyof T>(
     instance: T,
-    item: K,
-    callback: (data: T[K]) => void
+    callback?: (data: T) => void
+): void;
+export function unobserve<T extends Object, K extends keyof T>(
+    instance: T,
+    item?: K,
+    callback?: (data: T[K]) => void
+): void
+export function unobserve<T extends Object, K extends keyof T>(
+    instance: T,
+    item?: K,
+    callback?: (data: T[K]) => void
 ) {
+    if (!callback) {
+        return;
+    }
+
     const signals: Map<string, Signal<T[K]>> = Reflect.getMetadata(SIGNAL_KEY, instance);
     if (!signals) {
         return;
     }
-    const signal = signals.get(String(item));
-    if (!signal) {
+    if (item) {
+        const signal = signals.get(String(item));
+        if (!signal) {
+            return;
+        }
+        signal.unsubscribe(callback);
         return;
     }
-    return signal.unsubscribe(callback);
+    signals.forEach((signal) => {
+        signal.unsubscribe(callback);
+    });
 }
